2324535e355a1c6e712b6a6b22691035b95fd09c
Shamil Sharapudinov <shamil.sharapudinov@adv.ru>
2020-12-23 01:50:14 +0300

    lesson 1

--- task6.py
+++ task6.py
@@ -1,14 +1,10 @@
 a = int(input('a = '));
 b = int(input('b = '));
 
-d = 0
-while a < b:
-    a *= 1.1
+d = 1
+s = a
+while s < b:
+    s *= 1.1
     d += 1
 
 print(d)
-
-# правильный способ
-import math
-
-print(math.ceil(math.log(b / a, 1.1)))
