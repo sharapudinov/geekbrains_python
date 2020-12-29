860069ffaeea47ac3877c808d0d7cc4025f0298b
Shamil Sharapudinov <shamil.sharapudinov@adv.ru>
2020-12-29 18:45:06 +0300

    task5 refactor

--- lesson2/task5.py
+++ lesson2/task5.py
@@ -3,9 +3,9 @@ my_list = [7, 5, 3, 3, 2]
 new = int(input())
 
 for i in range(len(my_list)):
-    if my_list[i] <= new >= my_list[i + 1]:
+    if i == len(my_list) - 1:
+        my_list.append(new)
+    elif my_list[i] <= new >= my_list[i + 1]:
         my_list = my_list[:i] + [new] + my_list[i:]
         break
-    elif i == len(my_list) - 1:
-        my_list.append(new)
 print(my_list)
