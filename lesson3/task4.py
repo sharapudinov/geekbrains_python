41b43d23549bcd5f827c00f023abffaff26ff6e7
Shamil Sharapudinov <shamil.sharapudinov@adv.ru>
2020-12-30 02:07:30 +0300

    task4 refactor

--- lesson3/task4.py
+++ lesson3/task4.py
@@ -1,4 +1,4 @@
-def my_funk(x, y):
+def my_func(x, y):
     """возведение в целую степень"""
     result = 1;
     p = abs(y)
@@ -6,6 +6,14 @@ def my_funk(x, y):
         result *= x
         p -= 1
     return result if y > 0 else 1 / result
+
+
+from functools import reduce
+
+
+def my_func_pro(x, y):
+    """возведение в целую степень в одну строку"""
+    return 1 / my_func_pro(x, -y) if y < 0 else reduce(lambda x, y: x * y, [x for i in range(y)], 1)
 
 
 def main():
@@ -28,7 +36,8 @@ def main():
             print('нужно целое отрицательное число')
             continue
 
-    print(my_funk(x, y))
+    print(my_funk_pro(x, y))
+    print(my_funk_pro(1, 0))
 
 
 main()
