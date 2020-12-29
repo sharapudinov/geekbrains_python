18be384757d3e56647a611a0574591d1f8ea00f8
Shamil Sharapudinov <shamil.sharapudinov@adv.ru>
2020-12-29 18:08:14 +0300

    code style fix

--- lesson2/task2.py
+++ lesson2/task2.py
@@ -4,7 +4,6 @@ while True:
     if i == '`':
         break
     list.append(i)
-for i in range(0,len(list)-2,2):
-    list[i], list[i+1] = list[i+1], list[i]
+for i in range(0, len(list) - 2, 2):
+    list[i], list[i + 1] = list[i + 1], list[i]
 print(list)
-
--- lesson2/task6.py
+++ lesson2/task6.py
@@ -1,4 +1,4 @@
-catalog=[]
+catalog = []
 index = 1
 while True:
     name = input('название: ')
@@ -6,21 +6,21 @@ while True:
         break
     while True:
         price = int(input('цена: '))
-        if price >=0:
+        if price >= 0:
             break
         print('цена должна быть натуральным числом')
     while True:
-        quantity =int(input('количество: '))
-        if quantity>=0:
+        quantity = int(input('количество: '))
+        if quantity >= 0:
             break
         print('количество должно быть натуральным числом')
-    unit =input('ед: ')
-    catalog.append((index,{'название': name,'цена': price,'количество':quantity,'ед':unit}))
-    index+=1
+    unit = input('ед: ')
+    catalog.append((index, {'название': name, 'цена': price, 'количество': quantity, 'ед': unit}))
+    index += 1
 analitic = {}
 for product in catalog:
     for prop in product[1].keys():
-        prop_set=analitic.get(prop,set())
+        prop_set = analitic.get(prop, set())
         prop_set.add(product[1][prop])
         analitic[prop] = prop_set
 for prop in analitic.keys():
