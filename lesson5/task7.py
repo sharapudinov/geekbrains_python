2e9d16affc210b89d68ad0a7ce1448fd6183059a
Shamil Sharapudinov <sharapudinov.sh@adv.ru>
2021-01-16 23:17:13 +0300

    less5 json output added

--- lesson5/task7.py
+++ lesson5/task7.py
@@ -1,6 +1,6 @@
 from json import dump
 
-with open('companies.txt') as f:
+with open('companies.txt', 'r') as f:
     analytic = {line.split()[0]: float(line.split()[2]) - float(line.split()[3]) for line in f}
     analytic['avg_profit'] = sum([value for value in analytic.values() if value > 0], 0)
     print(analytic)
