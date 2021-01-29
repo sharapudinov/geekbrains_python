beae01043c502171a18ccf03c63827318e17d653
Shamil Sharapudinov <sharapudinov.sh@adv.ru>
2021-01-29 20:50:06 +0300

    fix

--- task1.py
+++ task1.py
@@ -1,4 +1,9 @@
 class Date:
+    def __init__(self, date: str):
+        date = Date.get(date)
+        if not Date.validate(**date):
+            raise ValueError
+        self.date = date
 
     @classmethod
     def get(cls, date: str):
@@ -11,12 +16,15 @@ class Date:
             print('месяц должеен быть числом от 1-12')
             return False
         days_in_month = [None, 31, 28 + int(not year % 4), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        if days_in_month[month] < day  or day < 0:
+        if days_in_month[month] < day or day < 0:
             print(f'В этом месяце не может быть {day}-е число')
             return False
         return True
 
+    def __str__(self):
+        return str(self.date)
 
-print(Date.get('01-01-01'))
+
+print(Date('31-09-061'))
 
 print(Date.validate(day=29, month=2, year=2000))
